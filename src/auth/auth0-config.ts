75a1610d8b1762a473888ba09ba136f3daad4639
Carter Rabasa <carter.rabasa@gmail.com>
2020-05-12 15:56:48 -0700

    Update auth0-config.ts

--- src/auth/auth0-config.ts
+++ src/auth/auth0-config.ts
@@ -1,9 +1,9 @@
 const AUTH_CONFIG = {
-    domain: process.env.REACT_APP_DOMAIN,
-    clientId: process.env.REACT_APP_CLIENT_ID,
-    audience: process.env.REACT_APP_AUDIENCE,
-    useRefreshTokens: process.env.REACT_APP_USE_REFRESH_TOKENS,
-    cacheLocation: process.env.REACT_APP_CACHE_LOCATION,
+    domain: process.env.AUTH0_DOMAIN,
+    clientId: process.env.AUTH0_CLIENT_ID,
+    audience: process.env.AUTH0_AUDIENCE,
+    useRefreshTokens: process.env.AUTH0_USE_REFRESH_TOKENS,
+    cacheLocation: process.env.AUTH0_CACHE_LOCATION,
 };
 
 export default AUTH_CONFIG;
