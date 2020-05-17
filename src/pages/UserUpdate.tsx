dc23a1316f3b3cfb9c24a243acc0bf32f9dc84c5
Carter Rabasa <carter.rabasa@gmail.com>
2020-05-17 08:48:19 -0700

    re-located files

--- src/pages/UserUpdate.tsx
+++ src/pages/UserUpdate.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
-import { useUpdateUserMutation, useUserQuery } from '../../generated/graphql';
-import UserForm from './UserForm';
+import gql from 'graphql-tag';
+import { useUpdateUserMutation, useUserQuery } from '../generated/graphql';
+import UserForm from '../forms/UserForm';
 
-const UserFormContainer = () => {
+gql`
+    mutation UpdateUser($userInput: UserUpdateInput!, $id: String!) {
+        updateOneUser(data: $userInput, where: { id: $id }) {
+            id
+        }
+    }
+`;
+
+const UserUpdate = () => {
     // this will come from a URL param or the session
     const { id } = useParams();
 
     const { data: userData, error: userError, loading: userLoading } = useUserQuery({
         variables: {
-            id,
+            where: {
+                id,
+            },
         },
     });
 
@@ -31,7 +42,13 @@ const UserFormContainer = () => {
         return <div>Saving...</div>;
     }
 
-    return <UserForm mutation={updateUserMutation} userData={userData} />;
+    return (
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-3xl mx-auto">
+                <UserForm mutation={updateUserMutation} userData={userData} />
+            </div>
+        </div>
+    );
 };
 
-export default UserFormContainer;
+export default UserUpdate;
