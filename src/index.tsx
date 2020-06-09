1b2713d7e9f23dba77c28a7075382cfcfe7bc7e1
Carter Rabasa <carter.rabasa@gmail.com>
2020-06-09 11:17:48 -0700

    better paths

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import { Auth0Provider } from 'src/auth/auth0';
-import BareLayout from './layouts/BareLayout';
-import Routes from './Routes';
-import GraphQLProvider from './GraphQLProvider';
-import UserProvider from './UserProvider';
-import Error from './pages/Error';
-import './index.css';
+import BareLayout from 'src/layouts/BareLayout';
+import Routes from 'src/Routes';
+import GraphQLProvider from 'src/GraphQLProvider';
+import UserProvider from 'src/UserProvider';
+import Error from 'src/pages/Error';
+import 'src/index.css';
 
 const Catch = ({ children }) => {
     const location = useLocation();
