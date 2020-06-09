4cff0e7e4c26d87c3b3d83b36597adcdf97fe5ef
Carter Rabasa <carter.rabasa@gmail.com>
2020-06-09 11:24:19 -0700

    simplified layouts

--- src/layouts/BareLayout.tsx
+++ src/layouts/BareLayout.tsx
@@ -1,35 +1,29 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-
 import Footer from '../components/nav/Footer';
 import './DefaultLayout.css';
 
-const DefaultLayout = ({ component: Component, ...rest }) => {
+const DefaultLayout = ({ component: Component }) => {
     return (
-        <Route
-            {...rest}
-            render={(props) => (
-                <>
-                    <Helmet>
-                        <title>Fizbuz - Error Page</title>
-                    </Helmet>
-                    <div id="default">
-                        <nav className="bg-white shadow">
-                            <div className="max-w-full p-4">
-                                <Link to="/">
-                                    <img className="block lg:hidden h-8 w-auto" src="/logo.png" alt="logo" />
-                                </Link>
-                            </div>
-                        </nav>
-                        <section id="content" className="m-4">
-                            <Component {...props} />
-                        </section>
-                        <Footer />
+        <>
+            <Helmet>
+                <title>Fizbuz - Error Page</title>
+            </Helmet>
+            <div id="default">
+                <nav className="bg-white shadow">
+                    <div className="max-w-full p-4">
+                        <Link to="/">
+                            <img className="block lg:hidden h-8 w-auto" src="/logo.png" alt="logo" />
+                        </Link>
                     </div>
-                </>
-            )}
-        />
+                </nav>
+                <section id="content" className="m-4">
+                    <Component />
+                </section>
+                <Footer />
+            </div>
+        </>
     );
 };
 
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,43 +1,38 @@
 import React from 'react';
-import { Route, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import DefaultNav from '../components/nav/DefaultNav';
-import Alert from '../components/ui/Alert';
+import { Alert } from '../components/ui/Alert';
 import Footer from '../components/nav/Footer';
 import './DefaultLayout.css';
 
-const DefaultLayout = ({ component: Component, ...rest }) => {
+const DefaultLayout = ({ component: Component }) => {
     const location = useLocation();
     return (
-        <Route
-            {...rest}
-            render={(props) => (
-                <>
-                    <Helmet>
-                        <title>Fizbuz</title>
-                    </Helmet>
-                    <div id="default">
-                        <DefaultNav />
-                        <section
-                            id="alert"
-                            style={{
-                                padding: '1px',
-                            }}
-                        >
-                            {location && location.state && location.state.message && (
-                                <div style={{ padding: '10px' }}>
-                                    <Alert message={location.state.message} />
-                                </div>
-                            )}
-                        </section>
-                        <section id="content" className="m-4">
-                            <Component {...props} />
-                        </section>
-                        <Footer />
-                    </div>
-                </>
-            )}
-        />
+        <>
+            <Helmet>
+                <title>Fizbuz</title>
+            </Helmet>
+            <div id="default">
+                <DefaultNav />
+                <section
+                    id="alert"
+                    style={{
+                        padding: '1px',
+                    }}
+                >
+                    {location && location.state && location.state.message && (
+                        <div style={{ padding: '10px' }}>
+                            <Alert mode={location.state.mode} message={location.state.message} />
+                        </div>
+                    )}
+                </section>
+                <section id="content" className="m-4">
+                    <Component />
+                </section>
+                <Footer />
+            </div>
+        </>
     );
 };
 
