ad990eecdfb9bdcd1d6f6a27f32d8272842f5cbd
Carter Rabasa <carter.rabasa@gmail.com>
2020-06-09 11:18:19 -0700

    simplified, better paths

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,68 +1,62 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect, useLocation } from 'react-router-dom';
-import { useAuth0 } from './auth/auth0';
-import AppLayout from './layouts/AppLayout';
-import DefaultLayout from './layouts/DefaultLayout';
-import Callback from './pages/Callback';
-import OnboardingProfile from './pages/onboarding/Profile';
-import OnboardingSkills from './pages/onboarding/Skills';
-import Landing from './pages/Landing';
-import Home from './pages/Home';
-import Error from './pages/Error';
+import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
+import { useAuth0 } from 'src/auth/auth0';
+import AppLayout from 'src/layouts/AppLayout';
+import DefaultLayout from 'src/layouts/DefaultLayout';
+import Callback from 'src/pages/Callback';
+import OnboardingProfile from 'src/pages/onboarding/Profile';
+import OnboardingSkills from 'src/pages/onboarding/Skills';
+import Landing from 'src/pages/Landing';
+import Home from 'src/pages/home';
+import HomeProfile from 'src/pages/home/Profile';
+import Error from 'src/pages/Error';
+import { Mode } from 'src/components/ui/Alert';
 
 const NoMatch = () => {
     return <div>404, sorry!</div>;
 };
 
-const PrivateRoute = ({ component, ...rest }) => {
+const PrivateRoute = ({ component, layout: Layout = AppLayout, ...rest }) => {
     const location = useLocation();
     const { loading, isAuthenticated } = useAuth0();
     if (loading) {
         return <div>Loading...</div>;
     }
     return (
-        <Route
-            {...rest}
-            render={(props) =>
-                isAuthenticated /* && (admin === false || isAdminUser(user)) */ ? (
-                    <AppLayout component={component} {...props} />
-                ) : (
-                    <Redirect
-                        to={{
-                            pathname: '/',
-                            state: { message: 'Please Log In', from: location },
-                        }}
-                    />
-                )
-            }
-        />
+        <Route {...rest}>
+            {isAuthenticated /* && (admin === false || isAdminUser(user)) */ ? (
+                <Layout component={component} />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: '/',
+                        state: { message: 'Please Log In', mode: Mode.WARN, from: location },
+                    }}
+                />
+            )}
+        </Route>
     );
 };
 
 const PublicRoute = ({ component: Component, layout: Layout = DefaultLayout, ...rest }) => (
-    <Route {...rest} render={(props) => <Layout component={Component} {...props} />} />
+    <Route {...rest}>
+        <Layout component={Component} />
+    </Route>
 );
 
 function Routes() {
     return (
         <Switch>
-            <PublicRoute exact path="/" component={Landing} />
-            <PrivateRoute exact path="/callback" component={Callback} />
+            <Route exact path="/callback" component={Callback} />
             <PrivateRoute exact path="/onboarding/profile" component={OnboardingProfile} />
             <PrivateRoute exact path="/home" component={Home} />
+            <PrivateRoute exact path="/home/profile" component={HomeProfile} />
             <PrivateRoute exact path="/onboarding/skills" component={OnboardingSkills} />
+            <PublicRoute exact path="/" component={Landing} />
             <PublicRoute path="/error" component={Error} />
             <PublicRoute path="*" component={NoMatch} />
         </Switch>
     );
 }
 
-function App() {
-    return (
-        <>
-            <Routes />
-        </>
-    );
-}
-
-export default App;
+export default Routes;
