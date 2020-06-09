6b65526a8fcc5b740fdfb0aa31eeaa9440bb3604
Carter Rabasa <carter.rabasa@gmail.com>
2020-06-09 11:20:48 -0700

    support mode

--- src/components/ui/Alert.tsx
+++ src/components/ui/Alert.tsx
@@ -1,37 +1,64 @@
 import * as React from 'react';
+import { SuccessIcon, ErrorIcon, WarnIcon, InfoIcon } from './Icons';
 
+enum Mode {
+    INFO,
+    WARN,
+    ERROR,
+    SUCCESS,
+}
 interface AlertProps {
     message: string;
     title?: string;
+    mode?: Mode;
 }
 
-function Alert({ message, title }: AlertProps) {
+function Alert({ message, title = undefined, mode = Mode.INFO }: AlertProps) {
+    let bgcolor,
+        text,
+        Icon = undefined;
+
+    if (mode === Mode.SUCCESS) {
+        bgcolor = 'bg-green-50';
+        Icon = SuccessIcon;
+        text = 'text-green-800';
+    } else if (mode === Mode.ERROR) {
+        bgcolor = 'bg-red-50';
+        Icon = ErrorIcon;
+        text = 'text-red-800';
+    } else if (mode === Mode.WARN) {
+        bgcolor = 'bg-yellow-50';
+        Icon = WarnIcon;
+        text = 'text-yellow-800';
+    } else if (mode === Mode.INFO) {
+        bgcolor = 'bg-blue-50';
+        Icon = InfoIcon;
+        text = 'text-blue-800';
+    } else {
+        bgcolor = 'bg-white';
+        text = 'text-black';
+    }
+
     return (
-        <div className="rounded-md bg-yellow-50 p-4">
+        <div className={`rounded-md ${bgcolor} p-4`}>
             <div className="flex">
                 <div className="flex-shrink-0">
-                    <svg className="h-5 w-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
-                        <path
-                            fillRule="evenodd"
-                            d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                            clipRule="evenodd"
-                        />
-                    </svg>
+                    <Icon />
                 </div>
                 <div className="ml-3">
                     {title && (
                         <>
-                            <h3 className="text-sm leading-5 font-medium text-yellow-800">{title}</h3>
-                            <div className="mt-2 text-sm leading-5 text-yellow-700">
+                            <h3 className={`text-sm leading-5 font-medium ${text}`}>{title}</h3>
+                            <div className={`mt-2 text-sm leading-5 ${text}`}>
                                 <p>{message}</p>
                             </div>
                         </>
                     )}
-                    {!title && <p className="text-sm leading-5 text-yellow-700">{message}</p>}
+                    {!title && <p className={`text-sm leading-5 ${text}`}>{message}</p>}
                 </div>
             </div>
         </div>
     );
 }
 
-export default Alert;
+export { Alert, Mode };
