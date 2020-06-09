1604db6fdfc5633e51961b25b8e42e827cd0e87d
Carter Rabasa <carter.rabasa@gmail.com>
2020-06-09 07:18:50 -0700

    moved /home to its own dir, factored-out GraphQL fragment

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,22 +5,20 @@ import { useHomeQuery } from 'src/generated/graphql';
 gql`
     query Home {
         currentUser {
-            profile {
-                nickname
-                name
-                skills {
-                    tool {
-                        name
-                    }
-                }
-            }
+            ...FullProfile
         }
     }
 `;
+
 function Home() {
     const { data } = useHomeQuery();
 
-    return <pre>{JSON.stringify(data, null, 2)}</pre>;
+    return (
+        <>
+            <h1>Dashboard</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </>
+    );
 }
 
 export default Home;
